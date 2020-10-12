67d2141d9c919f521e5b808b9bdb3a792bdd0711
TimeL0bster <72735048+TimeL0bster@users.noreply.github.com>
2020-10-12 17:54:18 +0700

    Gizmos and Raycast

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+
+    [Header("Distance")]
+    public Vector3 Leight = Vector3.zero;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(transform.position, Vector3.back,5f);
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, Leight);
+    }
+}
