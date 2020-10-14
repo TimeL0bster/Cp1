cd72beb1eb3df20d5df336494efbfa15c843e985
hoangdv07241 <57866135+hoangdv07241@users.noreply.github.com>
2020-10-15 02:56:44 +0700

    Slots added.

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -14,23 +14,37 @@ public class Object : MonoBehaviour
     public LayerMask isObjectLayer;
 
     private Animator anim;
+    private Slots slots;
+
     public bool isObjectAbove = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        slots = GameObject.FindGameObjectWithTag("Slots").GetComponent<Slots>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isObjectAbove = Physics.Raycast(transform.position + collOffsetHorizontal, Vector3.back, Leight, isObjectLayer) || Physics.Raycast(transform.position - collOffsetHorizontal, Vector3.back, Leight, isObjectLayer);
-            //|| Physics.Raycast(transform.position + collOffsetVertical, Vector3.back, Leight, isObjectLayer) || Physics.Raycast(transform.position - collOffsetVertical, Vector3.back, Leight, isObjectLayer);
+        isObjectAbove = Physics.Raycast(transform.position + collOffsetHorizontal, Vector3.back, Leight, isObjectLayer) || Physics.Raycast(transform.position - collOffsetHorizontal, Vector3.back, Leight, isObjectLayer)
+            || Physics.Raycast(transform.position + collOffsetVertical, Vector3.back, Leight, isObjectLayer) || Physics.Raycast(transform.position - collOffsetVertical, Vector3.back, Leight, isObjectLayer);
 
         if (isObjectAbove)
         {
             Debug.Log("Touched");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        for (int i = 0;i < slots.slots.Length;i++)
+        {
+            if (slots.isFull[i] == false)
+            {
+                slots.isFull[i] = true;
+                break;
+            }
         }
     }
 
