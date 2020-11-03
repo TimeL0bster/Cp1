1d9c5d5c393469c378e267d0d24d5ea4b0de52f8
TimeL0bster <72735048+TimeL0bster@users.noreply.github.com>
2020-11-03 12:02:07 +0700

    Object swap position (Incomplete)
    Mini object able to swap position. (Incomplete)

--- Assets/Scripts/MiniObject.cs
+++ Assets/Scripts/MiniObject.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class MiniObject : MonoBehaviour
 {
     [Header("Distance")]
-    public int leight = 0;
+    public int leight1 = 0;
+    public int leight2 = 0;
     public Vector3 collOffsetHorizontal;
     public Vector3 collOffsetVertical;
     public Vector3 collOffsetHorizontal2;
@@ -15,9 +16,13 @@ public class MiniObject : MonoBehaviour
     public Vector3 collOffsetHorizontal3;
     public Vector3 collOffsetVertical3;
 
+    [Header("Sprites")]
+    public Sprite[] miniObjectSprite;
+
     protected Image img;
 
     private string mySlot = null;
+    private float i = 0f;
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -27,24 +32,35 @@ public class MiniObject : MonoBehaviour
 
     protected void detechAdjacientSlot()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + collOffsetVertical + collOffsetHorizontal,Vector3.left * leight);
+        RaycastHit2D miniObjectHitUpperLeft = Physics2D.Raycast(transform.position + collOffsetVertical + collOffsetHorizontal, Vector3.left * leight1);
+        RaycastHit2D miniObjectHitLowerLeft = Physics2D.Raycast(transform.position + collOffsetVertical2 + collOffsetHorizontal2, Vector3.left * leight2);
+        List<GameObject> objectHit = new List<GameObject>();
 
-        while (hit.collider != null && hit.collider.GetComponent<Image>().sprite == img.sprite)
+        if (miniObjectHitLowerLeft.collider != null && miniObjectHitLowerLeft.collider.GetComponent<Image>().sprite == miniObjectSprite[0])
         {
-            mySlot = hit.collider.transform.parent.name;
-            hit = Physics2D.Raycast(hit.collider.transform.position + collOffsetVertical + collOffsetHorizontal, Vector3.left * leight);
+            /*objectHit.Add(miniObjectHit.collider.gameObject);
+            mySlot = miniObjectHit.collider.transform.parent.name;
             Debug.Log(mySlot);
+            */
+            if (miniObjectHitUpperLeft.collider != null && miniObjectHitUpperLeft.collider.GetComponent<Image>().sprite == img.sprite)
+            {
+                i = Time.deltaTime / .2f;
+                transform.SetParent(miniObjectHitLowerLeft.collider.transform.parent);
+                transform.position = Vector3.Lerp(transform.position, miniObjectHitLowerLeft.collider.transform.position, i);
+            }
+
         }
+
     }
 
     protected void SwitchObjectPosition()
     {
-
+        //transform.position = Vector3.Lerp();
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
-        Gizmos.DrawLine(transform.position + collOffsetVertical + collOffsetHorizontal, transform.position + collOffsetVertical + collOffsetHorizontal + Vector3.left * leight);
+        Gizmos.DrawLine(transform.position + collOffsetVertical + collOffsetHorizontal, transform.position + collOffsetVertical + collOffsetHorizontal + Vector3.left * leight1);
     }
 }
