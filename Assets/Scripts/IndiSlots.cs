845719364bab8be6d37ba608fa8a9d5a98dca682
TimeL0bster <72735048+TimeL0bster@users.noreply.github.com>
2020-12-01 03:24:13 +0700

    1
    finish randomobjgen & objposwtch

--- Assets/Scripts/IndiSlots.cs
+++ Assets/Scripts/IndiSlots.cs
@@ -7,7 +7,7 @@ public class IndiSlots : MonoBehaviour
 {
 
     public int i;
-    public GameObject[] previousSlots;
+    public GameObject[] otherSlots;
     public GameObject[] miniObj;
 
     private Slots slots;
@@ -27,6 +27,48 @@ public class IndiSlots : MonoBehaviour
         if (transform.childCount <= 0)
         {
             slots.isFull[i] = false;
+        }
+
+        switch (gameObject.tag)
+        {
+            case "Slot1":
+
+                if (transform.GetChild(0).transform.GetComponent<Image>().sprite == otherSlots[1].transform.GetChild(0).GetComponent<Image>().sprite)
+                {
+                    Debug.Log("Match1");
+                }
+                break;
+
+            case "Slot2":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
+            case "Slot3":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
+            case "Slot4":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
+            case "Slot5":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
+            case "Slot6":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
+            case "Slot7":
+
+                Debug.Log("Name:" + transform.name);
+                break;
+
         }
 
     }
