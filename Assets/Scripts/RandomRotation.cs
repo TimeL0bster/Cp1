a2713e7e0e29502e9aec1a50c0bf4af57b132c8b
hoangdv07241 <57866135+hoangdv07241@users.noreply.github.com>
2020-12-15 13:18:10 +0700

    implement 3d objs

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+
+    private Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = new Vector3(Random.Range(-78,-120), 0, Random.Range(120, 65));
+        transform.eulerAngles = rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
